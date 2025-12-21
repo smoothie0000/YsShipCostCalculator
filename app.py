3c952d7b9f0e41efca9ab15d3d1edf4aa6e01056
Weihan <zuweihan1992@gmail.com>
2025-12-21 09:40:18 +0100

    remove debang and shunxin

--- app.py
+++ app.py
@@ -35,18 +35,18 @@ class App(ctk.CTk):
         }
 
         self.shipping_strategies = {
-            "德邦": shipping_strategy.DebangShippingStrategy(),
+            # "德邦": shipping_strategy.DebangShippingStrategy(),
             # "极兔": shipping_strategy.JituShippingStrategy(),
             "韵达": shipping_strategy.YundaShippingStrategy(),
             "邮政-电商标快洪家": shipping_strategy.YouzhengDianShangShippingStrategy(),
             "邮政-EMS洪家": shipping_strategy.YouzhengEmsShippingStrategy(),
             # "邮政-小包": shipping_strategy.YouzhengXiaoBaoShippingStrategy(),
-            "快运-顺心捷达": shipping_strategy.KuaiyunShunXinShippingStrategy(),
+            # "快运-顺心捷达": shipping_strategy.KuaiyunShunXinShippingStrategy(),
+            "中通快运": shipping_strategy.ZhongTongKuaiYunShippingStrategy(),
             "快运-壹米滴答": shipping_strategy.KuaiyunYiMiShippingStrategy(),
             "快运-韵达快运": shipping_strategy.KuaiyunYunDaShippingStrategy(),
             "顺丰快递": shipping_strategy.ShunFengShippingStrategy(),
             "中通快递": shipping_strategy.ZhongTongKuaiDiShippingStrategy(),
-            "中通快运": shipping_strategy.ZhongTongKuaiYunShippingStrategy(),
         }
 
         self._build_ui()
